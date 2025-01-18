0864b9b14bb6f5361d6ebae1b1259248d593bfcb
Paul <phongcpt72@gmail.com>
2025-01-18 23:46:54 +0700

    update chain

--- src/apis/event/WebhookController.ts
+++ src/apis/event/WebhookController.ts
@@ -13,34 +13,34 @@ export class WebhookController {
     @Inject()
     private readonly WebhookService: WebhookService;
 
-    @Post()
-    async handleWebhook(@BodyParams() body: any, @Res() res: Response) {
-    try {
-        const providedSignature = res.req.headers["x-signature"]
-        const generatedSignature= Web3.utils.sha3(JSON.stringify(res.req.body)+process.env.MORALIS_STREAM_API_KEY)
+    // @Post()
+    // async handleWebhook(@BodyParams() body: any, @Res() res: Response) {
+    // try {
+    //     const providedSignature = res.req.headers["x-signature"]
+    //     const generatedSignature= Web3.utils.sha3(JSON.stringify(res.req.body)+process.env.MORALIS_STREAM_API_KEY)
         
-        // console.log(generatedSignature)
-        // console.log(providedSignature)
-        if(body.abi.length != 0)
-            {
+    //     // console.log(generatedSignature)
+    //     // console.log(providedSignature)
+    //     if(body.abi.length != 0)
+    //         {
                 
-                if (body.confirmed && generatedSignature === providedSignature)
-                {
-                    await this.WebhookService.handleWebhook(body)
-                }
-            }
-        else
-        {
-            if (generatedSignature === providedSignature)
-            {
-                return res.status(200).json({ message: "Webhook received successfully" });
-            }
-        }
-    } catch (e) {
-        console.error(e);
-        return res.status(400).json({ error: "Failed to process webhook" });
-    }
-    }
+    //             if (body.confirmed && generatedSignature === providedSignature)
+    //             {
+    //                 await this.WebhookService.handleWebhook(body)
+    //             }
+    //         }
+    //     else
+    //     {
+    //         if (generatedSignature === providedSignature)
+    //         {
+    //             return res.status(200).json({ message: "Webhook received successfully" });
+    //         }
+    //     }
+    // } catch (e) {
+    //     console.error(e);
+    //     return res.status(400).json({ error: "Failed to process webhook" });
+    // }
+    // }
 
 }
 
